04fed9430ecf25c519e61290bd2a88e713054ec5
Oscar Hall <oscar.hall@outlook.com>
2021-05-15 12:35:21 +0200

    Initial socket OSAL implemented in tcp_server_socket

--- src/network/tcp_server_socket.cpp
+++ src/network/tcp_server_socket.cpp
@@ -1,16 +1,7 @@
 #include "tcp_server_socket.h"
 
-#ifdef WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <winsock2.h>
-#include <stdio.h>
-typedef int socklen_t;
-#else
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h> //close
-#endif
+#include "osal.h"
+
 
 namespace hallos
 {
@@ -20,43 +11,25 @@ namespace hallos
  */ 
 tcp_server_socket::tcp_server_socket(int port)
 {
-#if WIN32
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0)
-    {
-        throw tcp_socket_exception("Failed to startup winsock.");
-    }
-    if (!(LOBYTE(wsaData.wVersion) >= 2))
-    {
-        throw tcp_socket_exception("Too old winsock version.");
-    }
-#endif //WIN32
-
-    socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    socket_ = os::socket_create(os::TCP);
     if (socket_ == INVALID_SOCKET)
     {
         throw tcp_socket_exception("Could not create socket.");
     } 
 
-    linger so_linger = {1, 0};
-    setsockopt(socket_, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger));
+   os::socket_set_so_linger(socket_, 1, 0);
 
-    sockaddr_in sockAdr = {0};
-    sockAdr.sin_family = AF_INET;
-    sockAdr.sin_port = htons(port);  
-    sockAdr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(socket_, reinterpret_cast<sockaddr*>(&sockAdr), sizeof(sockAdr)) != 0)
+    if(!os::socket_bind(socket_, port))
     {
-        closeSocket();
+        os::socket_close(socket_);
         throw tcp_socket_exception("Could not bind socket.");
     }
 
-    if (listen(socket_, SOMAXCONN)!=0)
+    if (!os::socket_listen(socket_))
     {
-        closeSocket();
+        os::socket_close(socket_);
         throw tcp_socket_exception("Could not set socket in listening mode.");
-    }   
+    }
 }
 
 /**
@@ -64,7 +37,7 @@ tcp_server_socket::tcp_server_socket(int port)
  */ 
 tcp_server_socket::~tcp_server_socket()
 {
-    closeSocket();
+    os::socket_close(socket_);
 }
 
 /**
@@ -72,29 +45,7 @@ tcp_server_socket::~tcp_server_socket()
  */
 std::unique_ptr<Itcp_stream_socket> tcp_server_socket::acceptConnection()
 {
-    sockaddr_in clientSockAdr;
-    socklen_t clientSockSize = sizeof(clientSockAdr);
-
-    /*fd_set fdRead;
-    FD_ZERO(&fdRead);
-    FD_SET(socket_,&fdRead);
-    struct timeval timeOutTime;// = {timeout,0};
-
-    timeOutTime.tv_sec = timeout;
-    timeOutTime.tv_usec = 0;
-
-    int retVal = select(1,&fdRead,NULL,NULL,&timeOutTime);
-    if (retVal <= 0)
-    {
-        return std::unique_ptr<tcp_stream_socket>();
-    }
-
-    if (!FD_ISSET(socket_,&fdRead))
-    {
-        return std::unique_ptr<tcp_stream_socket>();
-    }*/
-
-    Socket clientSocket = accept(socket_, reinterpret_cast<sockaddr*>(&clientSockAdr),&clientSockSize);
+    Socket clientSocket = os::socket_accept(socket_);
     if (clientSocket == INVALID_SOCKET)
     {
         return std::unique_ptr<tcp_stream_socket>();
@@ -102,18 +53,5 @@ std::unique_ptr<Itcp_stream_socket> tcp_server_socket::acceptConnection()
 
     return std::make_unique<tcp_stream_socket>(clientSocket);    
 } 
-
-void tcp_server_socket::closeSocket()
-{
-    if (socket_ != INVALID_SOCKET)
-    {
-#if WIN32
-        closesocket(socket_);
-        WSACleanup();
-#else
-        close(socket_);
-#endif
-    }
-}
 
 } //namespace hallos
---
+++ src/osal.h
@@ -0,0 +1,30 @@
+#ifndef HALLOS_OSAL_H_INCLUDED
+#define HALLOS_OSAL_H_INCLUDED
+
+namespace hallos
+{
+namespace os
+{
+
+enum socket_type
+{
+    TCP,
+    UDP
+};
+
+int socket_create(socket_type type);
+
+bool socket_bind(int socket, int port);
+
+bool socket_listen(int socket);
+
+void socket_close(int socket);
+
+int socket_accept(int socket);
+
+int socket_set_so_linger(int socket, int arg1, int arg2);
+
+}; // namespace os
+}; // namespace hallos
+
+#endif
